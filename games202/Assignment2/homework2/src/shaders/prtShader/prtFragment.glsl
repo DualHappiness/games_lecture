95ea69daa8794dce60750c6ee9a57f4b55d40570
dual <dualwu_tech@outlook.com>
2021-06-08 16:19:55 +0800

    gamma correct

--- games202/Assignment2/homework2/src/shaders/prtShader/prtFragment.glsl
+++ games202/Assignment2/homework2/src/shaders/prtShader/prtFragment.glsl
@@ -5,5 +5,6 @@ precision mediump float;
 varying highp vec3 vColor;
 
 void main(void) {
-  gl_FragColor = vec4(vColor, 1.0);
+  vec3 color = pow(clamp(vColor, vec3(0.0), vec3(1.0)), vec3(1.0 / 2.2));
+  gl_FragColor = vec4(color, 1.0);
 }
