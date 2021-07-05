801bdb3976fa4c1f5e1533c3e1093ad44b8b5805
dual <dualwu_tech@outlook.com>
2021-07-05 18:03:50 +0800

    bounce1 done

--- games202/homework4/homework4/src/shaders/pbrShader/PBRFragment.glsl
+++ games202/homework4/homework4/src/shaders/pbrShader/PBRFragment.glsl
@@ -19,62 +19,73 @@ varying highp vec3 vNormal;
 
 const float PI = 3.14159265359;
 
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-   // TODO: To calculate GGX NDF here
-    
+float DistributionGGX(vec3 N, vec3 H, float roughness) {
+    // To calculate GGX NDF here
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(0.0, dot(H, N));
+    float NdotH2 = NdotH * NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+    return nom / max(denom, 0.0001);
 }
 
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    // TODO: To calculate Smith G1 here
-    
-    return 1.0;
+float GeometrySchlickGGX(float NdotV, float roughness) {
+    // To calculate Smith G1 here
+    float a = roughness;
+    float k = (a * a) / 2.0;
+
+    float nom = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
 }
 
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    // TODO: To calculate Smith G here
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness) {
+    // To calculate Smith G here
+    float ggx1 = GeometrySchlickGGX(dot(N, L), roughness);
+    float ggx2 = GeometrySchlickGGX(dot(N, V), roughness);
 
-    return 1.0;
+    return ggx1 * ggx2;
 }
 
-vec3 fresnelSchlick(vec3 F0, vec3 V, vec3 H)
-{
-    // TODO: To calculate Schlick F here
-    return vec3(1.0);
+vec3 fresnelSchlick(vec3 F0, vec3 V, vec3 H) {
+    // To calculate Schlick F here
+    return F0 + (1.0 - F0) * pow(1.0 - dot(H, V), 5.0);
 }
 
 void main(void) {
-  vec3 albedo = pow(texture2D(uAlbedoMap, vTextureCoord).rgb, vec3(2.2));
+    vec3 albedo = pow(texture2D(uAlbedoMap, vTextureCoord).rgb, vec3(2.2));
 
-  vec3 N = normalize(vNormal);
-  vec3 V = normalize(uCameraPos - vFragPos);
-  float NdotV = max(dot(N, V), 0.0);
- 
-  vec3 F0 = vec3(0.04); 
-  F0 = mix(F0, albedo, uMetallic);
+    vec3 N = normalize(vNormal);
+    vec3 V = normalize(uCameraPos - vFragPos);
+    float NdotV = max(dot(N, V), 0.0);
 
-  vec3 Lo = vec3(0.0);
+    vec3 F0 = vec3(0.04);
+    F0 = mix(F0, albedo, uMetallic);
 
-  vec3 L = normalize(uLightDir);
-  vec3 H = normalize(V + L);
-  float NdotL = max(dot(N, L), 0.0); 
+    vec3 Lo = vec3(0.0);
 
-  vec3 radiance = uLightRadiance;
+    vec3 L = normalize(uLightDir);
+    vec3 H = normalize(V + L);
+    float NdotL = max(dot(N, L), 0.0);
 
-  float NDF = DistributionGGX(N, H, uRoughness);   
-  float G   = GeometrySmith(N, V, L, uRoughness); 
-  vec3 F = fresnelSchlick(F0, V, H);
-      
-  vec3 numerator    = NDF * G * F; 
-  float denominator = max((4.0 * NdotL * NdotV), 0.001);
-  vec3 BRDF = numerator / denominator;
+    vec3 radiance = uLightRadiance;
 
-  Lo += BRDF * radiance * NdotL;
-  vec3 color = Lo;
+    float NDF = DistributionGGX(N, H, uRoughness);
+    float G = GeometrySmith(N, V, L, uRoughness);
+    vec3 F = fresnelSchlick(F0, V, H);
 
-  color = color / (color + vec3(1.0));
-  color = pow(color, vec3(1.0/2.2)); 
-  gl_FragColor = vec4(color, 1.0);
+    vec3 numerator = NDF * G * F;
+    float denominator = max((4.0 * NdotL * NdotV), 0.001);
+    vec3 BRDF = numerator / denominator;
+
+    Lo += BRDF * radiance * NdotL;
+    vec3 color = Lo;
+
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0 / 2.2));
+    gl_FragColor = vec4(color, 1.0);
 }
--- games202/homework4/lut-gen/Emu_IS.cpp
+++ games202/homework4/lut-gen/Emu_IS.cpp
@@ -13,7 +13,8 @@
 
 const int resolution = 128;
 
-Vec2f Hammersley(uint32_t i, uint32_t N) { // 0-1
+Vec2f Hammersley(uint32_t i, uint32_t N)
+{ // 0-1
     uint32_t bits = (i << 16u) | (i >> 16u);
     bits = ((bits & 0x55555555u) << 1u) | ((bits & 0xAAAAAAAAu) >> 1u);
     bits = ((bits & 0x33333333u) << 2u) | ((bits & 0xCCCCCCCCu) >> 2u);
@@ -23,41 +24,50 @@ Vec2f Hammersley(uint32_t i, uint32_t N) { // 0-1
     return {float(i) / float(N), rdi};
 }
 
-Vec3f ImportanceSampleGGX(Vec2f Xi, Vec3f N, float roughness) {
+Vec3f ImportanceSampleGGX(Vec2f Xi, Vec3f N, float roughness)
+{
     float a = roughness * roughness;
 
-    //TODO: in spherical space - Bonus 1
+    // in spherical space - Bonus 1
+    // from spherical space to cartesian space - Bonus 1
+    float theta = std::atan2(a * std::sqrt(Xi.x), std::sqrt(1.0 - Xi.x));
+    float phi = 2.0 * PI * Xi.y;
 
+    // tangent coordinates - Bonus 1
+    // transform H to tangent space - Bonus 1
+    Vec3f H = Vec3f(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
 
-    //TODO: from spherical space to cartesian space - Bonus 1
- 
-
-    //TODO: tangent coordinates - Bonus 1
-
-
-    //TODO: transform H to tangent space - Bonus 1
-    
-    return Vec3f(1.0f);
+    return H;
 }
 
-float GeometrySchlickGGX(float NdotV, float roughness) {
-    // TODO: To calculate Schlick G1 here - Bonus 1
-    
-    return 1.0f;
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    // To calculate Schlick G1 here - Bonus 1
+    float a = roughness;
+    float k = (a * a) / 2.0f;
+
+    float nom = NdotV;
+    float denom = NdotV * (1.0f - k) + k;
+
+    return nom / denom;
+    ;
 }
 
-float GeometrySmith(float roughness, float NoV, float NoL) {
+float GeometrySmith(float roughness, float NoV, float NoL)
+{
     float ggx2 = GeometrySchlickGGX(NoV, roughness);
     float ggx1 = GeometrySchlickGGX(NoL, roughness);
 
     return ggx1 * ggx2;
 }
 
-Vec3f IntegrateBRDF(Vec3f V, float roughness) {
-
+Vec3f IntegrateBRDF(Vec3f V, float roughness)
+{
     const int sample_count = 1024;
     Vec3f N = Vec3f(0.0, 0.0, 1.0);
-    for (int i = 0; i < sample_count; i++) {
+    float irr = 0.0;
+    for (int i = 0; i < sample_count; i++)
+    {
         Vec2f Xi = Hammersley(i, sample_count);
         Vec3f H = ImportanceSampleGGX(Xi, N, roughness);
         Vec3f L = normalize(H * 2.0f * dot(V, H) - V);
@@ -66,22 +76,25 @@ Vec3f IntegrateBRDF(Vec3f V, float roughness) {
         float NoH = std::max(H.z, 0.0f);
         float VoH = std::max(dot(V, H), 0.0f);
         float NoV = std::max(dot(N, V), 0.0f);
-        
-        // TODO: To calculate (fr * ni) / p_o here - Bonus 1
 
+        // To calculate (fr * ni) / p_o here - Bonus 1
+        float weight = VoH * GeometrySmith(roughness, NoV, NoL) / (NoV * NoH);
+        irr += weight;
 
         // Split Sum - Bonus 2
-        
     }
 
-    return Vec3f(1.0f);
+    return Vec3f(irr / sample_count);
 }
 
-int main() {
+int main()
+{
     uint8_t data[resolution * resolution * 3];
     float step = 1.0 / resolution;
-    for (int i = 0; i < resolution; i++) {
-        for (int j = 0; j < resolution; j++) {
+    for (int i = 0; i < resolution; i++)
+    {
+        for (int j = 0; j < resolution; j++)
+        {
             float roughness = step * (static_cast<float>(i) + 0.5f);
             float NdotV = step * (static_cast<float>(j) + 0.5f);
             Vec3f V = Vec3f(std::sqrt(1.f - NdotV * NdotV), 0.f, NdotV);
@@ -95,7 +108,7 @@ int main() {
     }
     stbi_flip_vertically_on_write(true);
     stbi_write_png("GGX_E_LUT.png", resolution, resolution, 3, data, resolution * 3);
-    
+
     std::cout << "Finished precomputed!" << std::endl;
     return 0;
 }
